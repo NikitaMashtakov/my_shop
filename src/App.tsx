7bde0e71d9698bb1e835b6b7f5c4984bc2161004
Маштаков Никита Сергеевич <NMashtakov@glx.local>
2023-11-28 17:38:37 +0700

    smth
    from work

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
+import { Product } from './components/Product';
+import axios from 'axios'
+import { AProduct } from './models';
+import { useEffect } from 'react';
 
 function App() {
-    return (
-        <React.Fragment>
-            <Header />
-            <Content />
-            <Footer />
-        </React.Fragment>
-    );
+  async function fetchProducts() {
+    const response = await axios.get<AProduct[]>('https://fakestoreapi.com/products')
+    console.log(response)
+  }
+  useEffect(()=>{console.log('effece')}, [])
+
+  return (
+    <div></div>
+  );
 }
 
 export default App;
