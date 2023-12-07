84b445be0b8f3b8b1913e79528737f7d65052804
Маштаков Никита Сергеевич <NMashtakov@glx.local>
2023-12-07 16:25:11 +0700

    table

--- src/components/ProductInCart.tsx
+++ src/components/ProductInCart.tsx
@@ -25,31 +25,49 @@ export function ProductInCart({product,quantity}: ICartProduct, key:any) {
         
         <div
         key={product.id}
-        className="border py-2 px-4 rounded flex h-[100px] mb-2"
+        className="border py-2 px-4 rounded flex h-[100px] mb-2 justify-center"
         >
-            <div className="grow flex justify-center items-center "><img className="h-[80px]" src={product.image} /></div>
-            <div className="text-center justify-center items-center"><p>{product.title}</p></div>
-            <div className="font-bold self-center justify-center items-center"> {product.price} </div>
-            <button 
-                className="border-2 py-2 px-4 rounded-lg bg-slate-500 text-white justify-center items-center"
-                onClick={clickMinus}
-            >
-                -
-            </button>
-            <div className="font-bold self-center justify-center items-center">{quantity}</div>
-            <button 
-                className="border-2 py-2 px-4 rounded-lg bg-slate-500 text-white justify-center items-center" 
-                onClick={clickPlus}
-            >
-                    +
-            </button>
-            <div className="font-bold self-center justify-center items-center"> {product.price*quantity} </div>
-            <button 
-                className="border-2 py-2 px-4 rounded-lg bg-slate-500 text-white justify-center items-center" 
-                onClick={clickRemove}
-            >
-                    Remove
-            </button>
+            <table className="table-auto">
+                <tr>
+                    <td><div ><img className="h-[80px]" src={product.image} /></div></td>
+                    <td> <div className="mr-10 ml-10 font-bold"><p>{product.title}</p></div></td>
+                    <td><div className="mr-10 ml-10 font-bold">${product.price} </div></td>
+                    <td>
+                        <button 
+                            className="border-1 py-2 px-4 rounded-full bg-gray-400 hover:bg-gray-600 text-white justify-center items-center"
+                            onClick={clickMinus}
+                        >
+                            -
+                        </button>
+                    </td>
+                    <td><div className="mr-5 ml-5">{quantity}</div></td>
+                    <td>
+                        <button 
+                        className="border-1 py-2 px-4 rounded-full bg-gray-400 hover:bg-gray-600 text-white justify-center items-center" 
+                        onClick={clickPlus}
+                        >
+                            +
+                        </button>
+                    </td>
+                    <td><div className="mr-10 ml-10 font-bold">${(product.price*quantity).toFixed(2)} </div></td>
+                    <td>
+                        <button 
+                            className="border-1 py-2 px-4 rounded-lg bg-gray-300 hover:bg-gray-600 text-white justify-center items-center" 
+                            onClick={clickRemove}
+                        >
+                            Remove
+                        </button>
+                    </td>
+                </tr>
+            </table>
+            
+           
+            
+            
+            
+            
+            
+           
         </div>
     )
 }
